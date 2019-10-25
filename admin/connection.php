02f6a2d9eb4d2c3a2c9a27c2ae18be4c308c0b83
Liên <51728184+nguyenlien1999@users.noreply.github.com>
2019-10-25 16:34:56 +0700

    vừa xóa admin cũ

--- admin/connection.php
+++
@@ -1,8 +0,0 @@
-<?php  
-	$server="localhost";
-	$user = "root";
-	$pass="";
-	$database="daihoctonducthang";
-	$conn = @mysqli_connect($server,$user,$pass,$database) or die("Khong ket noi dc");
-	mysqli_set_charset($conn,"utf8");
-?>
--- admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-session_start();
-unset($_SESSION['current_user']);
-header("location:login.php");
- ?>
